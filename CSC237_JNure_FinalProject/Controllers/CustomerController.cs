4d9d3ca623961bba2a29ffbe2e7067a7f2367145
jabir nure <jabir.aman@ymail.com>
2021-03-10 21:27:54 -0700

    Added views and controller

---
+++ CSC237_JNure_FinalProject/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSC237_JNure_FinalProject.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CSC237_JNure_FinalProject/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSC237_JNure_FinalProject.Models
+{
+    public class Customer
+    {
+
+
+    }
+}
